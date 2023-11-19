64df739ccb5f04ca9a840da1e74ef9f8f462e94f
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-11-19 17:02:05 +0800

    Add project files.

---
+++ Digital Image Processing/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Digital_Image_Processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnUpload = new Button();
+            btnGray = new Button();
+            btnToggleCamera = new Button();
+            btnNoFilter = new Button();
+            btnHistogram = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(32, 33);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(468, 398);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(549, 33);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(468, 398);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // btnUpload
+            // 
+            btnUpload.Location = new Point(138, 446);
+            btnUpload.Name = "btnUpload";
+            btnUpload.Size = new Size(94, 53);
+            btnUpload.TabIndex = 2;
+            btnUpload.Text = "Upload Image";
+            btnUpload.UseVisualStyleBackColor = true;
+            btnUpload.Click += btnUpload_Click;
+            // 
+            // btnGray
+            // 
+            btnGray.Location = new Point(650, 446);
+            btnGray.Name = "btnGray";
+            btnGray.Size = new Size(94, 29);
+            btnGray.TabIndex = 3;
+            btnGray.Text = "Gray";
+            btnGray.UseVisualStyleBackColor = true;
+            btnGray.Click += btnGray_Click;
+            // 
+            // btnToggleCamera
+            // 
+            btnToggleCamera.Location = new Point(284, 446);
+            btnToggleCamera.Name = "btnToggleCamera";
+            btnToggleCamera.Size = new Size(94, 53);
+            btnToggleCamera.TabIndex = 4;
+            btnToggleCamera.Text = "Use Camera";
+            btnToggleCamera.UseVisualStyleBackColor = true;
+            btnToggleCamera.Click += btnToggleCamera_Click;
+            // 
+            // btnNoFilter
+            // 
+            btnNoFilter.Location = new Point(550, 446);
+            btnNoFilter.Name = "btnNoFilter";
+            btnNoFilter.Size = new Size(94, 29);
+            btnNoFilter.TabIndex = 5;
+            btnNoFilter.Text = "No Filter";
+            btnNoFilter.UseVisualStyleBackColor = true;
+            btnNoFilter.Click += btnNoFilter_Click;
+            // 
+            // btnHistogram
+            // 
+            btnHistogram.Location = new Point(750, 446);
+            btnHistogram.Name = "btnHistogram";
+            btnHistogram.Size = new Size(94, 29);
+            btnHistogram.TabIndex = 6;
+            btnHistogram.Text = "Histogram";
+            btnHistogram.UseVisualStyleBackColor = true;
+            btnHistogram.Click += btnHistogram_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1047, 556);
+            Controls.Add(btnHistogram);
+            Controls.Add(btnNoFilter);
+            Controls.Add(btnToggleCamera);
+            Controls.Add(btnGray);
+            Controls.Add(btnUpload);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button btnUpload;
+        private Button btnGray;
+        private Button btnToggleCamera;
+        private Button btnNoFilter;
+        private Button btnHistogram;
+    }
+}
---
+++ Digital Image Processing/Form1.cs
@@ -0,0 +1,385 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace Digital_Image_Processing
+{
+    public partial class Form1 : Form
+    {
+        private bool isCameraView = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (IsImageFile(openFile.FileName))
+                    {
+                        // load img
+                        Image loadedImage = Image.FromFile(openFile.FileName);
+
+                        // set image size to fit the pic box
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                        // load picbox1
+                        pictureBox1.Image = loadedImage;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid image file selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        //CHECKPOINT!!!!!!!!!!!!!!!!!!!!!!!
+        //Exception handling codes!! Must put para no error ig run
+
+        //Exception 1: Wrong image format prompt
+        private bool IsImageFile(string filePath)
+        {
+            try
+            {
+                using (var bitmap = new Bitmap(filePath))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //STOP camera run after exit spp
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+
+
+        //CAMERA LIVE FEED DISPLAY
+        private void btnToggleCamera_Click(object sender, EventArgs e)
+        {
+            if (isCameraView)
+            {
+                // CAMERA --> FILE UPLOAD
+                isCameraView = false;
+                btnUpload.Enabled = true; // upload enable
+                btnToggleCamera.Text = "Switch to Camera View";
+                pictureBox1.Image = null; // Clear 
+                StopCameraView();
+
+            }
+            else
+            {
+                // FILE UPLOAD --> CAMERA
+                isCameraView = true;
+                btnUpload.Enabled = false; // Disable upload
+                btnToggleCamera.Text = "Switch to File Upload View";
+                StartCameraView(); // display cam
+            }
+        }
+
+
+        private void StartCameraView()
+        {
+            // scan device camera
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count > 0)
+            {
+                //general camera
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.NewFrame += VideoSource_NewFrame;
+                videoSource.Start();
+            }
+            else
+            {
+                MessageBox.Show("No video devices found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StopCameraView()
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+
+        //CAMERA VIDEO CODES ARI
+
+
+        //private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        //{
+        //    // Handle each new frame from the camera here
+        //    // The event is triggered for each new frame received from the camera
+        //    Image cameraFrame = (Image)eventArgs.Frame.Clone();
+
+        //    // Update the PictureBox with the new frame
+        //    pictureBox1.Invoke((MethodInvoker)delegate
+        //    {
+        //        pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // Set SizeMode to Zoom
+        //        pictureBox1.Image = cameraFrame;
+
+        //        if (btnGray.Enabled == false)
+        //        {
+        //            Image grayscaleFrame = ConvertToGrayscale((Bitmap)cameraFrame.Clone());
+        //            Image copyFrame = CopyImage((Bitmap)cameraFrame.Clone());
+
+        //            // Decide which image to display in pictureBox2
+        //            if (btnNoFilter.Enabled == false)
+        //            {
+        //                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        //                pictureBox2.Image = copyFrame;
+        //            }
+        //            else
+        //            {
+        //                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        //                pictureBox2.Image = grayscaleFrame;
+        //            }
+        //        }
+        //    });
+        //}
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //CLONE CAM
+            Image cameraFrame = (Image)eventArgs.Frame.Clone();
+
+            // update picbox1
+            pictureBox1.Invoke((MethodInvoker)delegate
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // Set SizeMode to Zoom
+                pictureBox1.Image = cameraFrame;
+
+                if (btnNoFilter.Enabled == false)
+                {
+                    // If "Copy" button is enabled, display the live camera feed in pictureBox2
+                    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox2.Image = cameraFrame;
+                }
+                else if (btnGray.Enabled == false)
+                {
+                    // If "Gray" button is enabled, display the live grayscale feed in pictureBox2
+                    Image grayscaleFrame = ConvertToGrayscale((Bitmap)cameraFrame);
+                    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox2.Image = grayscaleFrame;
+                }
+            });
+        }
+
+
+
+
+
+
+        //IMAGE PROCESSING CODES ARI
+
+
+        //1. GRAYSCALE (credits to CITU DIP METHODS REPOSITORY)
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                // grayscale display to picbox2
+                Image grayscaleImage = ConvertToGrayscale((Bitmap)pictureBox1.Image.Clone());
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox2.Image = grayscaleImage;
+            }
+            else
+            {
+                MessageBox.Show("No image to process.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Image ConvertToGrayscale(Bitmap originalImage)
+        {
+            // Copy image
+            Bitmap grayscaleImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            //  LockBits
+            BitmapData originalData = originalImage.LockBits(new Rectangle(0, 0, originalImage.Width, originalImage.Height),
+                                                             ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData grayscaleData = grayscaleImage.LockBits(new Rectangle(0, 0, grayscaleImage.Width, grayscaleImage.Height),
+                                                               ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                byte* originalPtr = (byte*)originalData.Scan0.ToPointer();
+                byte* grayscalePtr = (byte*)grayscaleData.Scan0.ToPointer();
+
+                int pixelSize = 3; // 24bits/pixel RGB
+
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    for (int x = 0; x < originalImage.Width; x++)
+                    {
+                        byte blue = originalPtr[0];
+                        byte green = originalPtr[1];
+                        byte red = originalPtr[2];
+
+                        // GRAYSCALE FORMULA
+                        byte grayscaleValue = (byte)(red * 0.3 + green * 0.59 + blue * 0.11);
+
+                        // SET PIXEL
+                        grayscalePtr[0] = grayscalePtr[1] = grayscalePtr[2] = grayscaleValue;
+
+                        originalPtr += pixelSize;
+                        grayscalePtr += pixelSize;
+                    }
+
+                    originalPtr += originalData.Stride - originalImage.Width * pixelSize;
+                    grayscalePtr += grayscaleData.Stride - grayscaleImage.Width * pixelSize;
+                }
+            }
+
+            // Unlock bits
+            originalImage.UnlockBits(originalData);
+            grayscaleImage.UnlockBits(grayscaleData);
+
+            return grayscaleImage;
+        }
+
+
+        //2. COPY OR NO FILTER
+        private void btnNoFilter_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+
+                Image copyImage = CopyImage((Bitmap)pictureBox1.Image.Clone());
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox2.Image = copyImage;
+            }
+            else
+            {
+                MessageBox.Show("No image to process.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Image CopyImage(Bitmap originalImage)
+        {
+            // Copy
+            Bitmap copyImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            // Iterate through each pixel in the original image
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    // GET PIXEL
+                    Color originalColor = originalImage.GetPixel(x, y);
+
+                    // SET PIXEL
+                    copyImage.SetPixel(x, y, originalColor);
+                }
+            }
+
+            return copyImage;
+        }
+
+
+
+        //3. COLOR INVERSION
+        //4. HISTOGRAM
+        private void btnHistogram_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+
+                Image histogramImage = HistogramImage((Bitmap)pictureBox1.Image.Clone());
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox2.Image = histogramImage;
+            }
+            else
+            {
+                MessageBox.Show("No image to process.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Image HistogramImage(Bitmap originalImage)
+        {
+            // Convert the original image to grayscale
+            Bitmap grayscaleImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color originalColor = originalImage.GetPixel(x, y);
+
+                    //            // GRAYSCALE FORMULA
+                                byte grayscaleValue = (byte)(originalColor.R * 0.3 + originalColor.G * 0.59 + originalColor.B * 0.11);
+
+                    //            // SET PIXEL
+                                grayscaleImage.SetPixel(x, y, Color.FromArgb(grayscaleValue, grayscaleValue, grayscaleValue));
+                }
+            }
+
+            // 1D histogram data
+            int[] histData = new int[256];
+            for (int x = 0; x < grayscaleImage.Width; x++)
+            {
+                for (int y = 0; y < grayscaleImage.Height; y++)
+                {
+                    Color sample = grayscaleImage.GetPixel(x, y);
+                    histData[sample.R]++;
+                }
+            }
+
+            // Display histogram image
+            Bitmap histogramImage = new Bitmap(256, 800);
+
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < 800; y++)
+                {
+                    histogramImage.SetPixel(x, y, Color.White);
+                }
+            }
+
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < Math.Min(histData[x] / 5, histogramImage.Height - 1); y++)
+                {
+                    histogramImage.SetPixel(x, (histogramImage.Height - 1) - y, Color.Black);
+                }
+            }
+
+            return histogramImage;
+        }
+
+
+        //5. SEPIA
+    }
+}
